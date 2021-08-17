0136698f8bc3a27d0c1b321bdacae506c3a8b8e3
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-08-17 16:41:51 +0600

    done

---
+++ string-reverse.js
@@ -0,0 +1,13 @@
+let greetings = "Hello, how are you?";
+
+function reverseString(text){
+    let reverse = "";
+    for(const letter of text){
+        reverse = letter + reverse;
+    }
+    return reverse;
+}
+
+const reversed = reverseString(greetings);
+console.log(greetings);
+console.log(reversed);
